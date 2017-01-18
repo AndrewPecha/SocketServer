90c74f10814ea692a5d4dc7f1e9417d572e85a6e
Andrew Pecha <andrew.pecha@gmail.com>
2017-01-17 20:28:35 -0600

    functional client app

---
+++ SocketServer/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    public class Server
+    {
+        public IPAddress LocalIP { get; set; }
+        public TcpListener Listener { get; set; }
+
+        public Server()
+        {
+            LocalIP = IPAddress.Any;
+            Listener = new TcpListener(LocalIP, 26969);
+        }
+
+        public void startListening()
+        {            
+            Listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Starting Listener...");
+
+                Socket clientSocket = Listener.AcceptSocket();
+
+                Console.WriteLine("A thing happened!");
+            }
+        }
+    }
+}
